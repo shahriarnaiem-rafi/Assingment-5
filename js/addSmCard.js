a947739f2cb6fe72b596adff8c76b0e98c9644f7
shahriarnaiem-rafi <shahriarnaiemrafi33@gmail.com>
2024-11-05 22:37:04 +0600

    card complete

--- js/addSmCard.js
+++ js/addSmCard.js
@@ -1,7 +1,17 @@
 document.getElementById('donate-btn')
 .addEventListener('click',function (event){
     event.preventDefault();
-    const inputAmount=getInputValueById('input=amount');
+    const inputAmount=getInputValueById('input-amount');
+    if(isNaN(inputAmount)){
+        alert("Money enter correctly");
+        return;
+    }
+
+    if(inputAmount<=0){
+        alert("please enter money");
+        return;
+    }
+
     const cardBalance=getFieldValueById('card-balance');
     const newBalance=inputAmount+cardBalance;
     document.getElementById('card-balance').innerText=newBalance;
--- js/utilities.js
+++ js/utilities.js
@@ -1,10 +1,12 @@
+// from input field
 function getInputValueById(id){
     const donateAmount=document.getElementById(id).value;
     const donateAmountNumber=parseFloat(donateAmount);
     return donateAmountNumber;
 }
+// from textfield
 function getFieldValueById(id){
     const cardBalance=document.getElementById(id).innerText;
     const cardBalanceNumber=parseFloat(cardBalance);
-    return cardBalanceNumber;
+    return cardBalanceNumber
 }
